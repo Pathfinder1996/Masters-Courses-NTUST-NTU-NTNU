a6aac114a9f08868efd510734a12f28a6046a332
Dylan Dai <142645742+Pathfinder1996@users.noreply.github.com>
2024-09-27 21:59:13 +0800

    Add SP HW

---
+++ fall_2024/system_software/hw1.asm
@@ -0,0 +1,45 @@
+        LDA     ONE
+        STA     I
+        LDA     ZERO
+        STA     INDEX
+        
+L1      LDA     ONE
+        STA     J
+
+L2      LDA     I
+        MUL     J
+        STA     SUM
+        LDA     INDEX
+        MUL     THREE
+        STA     TEMP
+        LDX     TEMP
+        LDA     SUM
+        STA     ARRAY, X
+        LDA     INDEX
+        ADD     ONE
+        STA     INDEX
+        LDA     J
+        ADD     ONE
+        STA     J
+        LDA     J
+        COMP    TEN
+        JLT     L2
+
+        LDA     I
+        ADD     ONE
+        STA     I
+        LDA     I
+        COMP    TEN
+        JLT     L1
+
+I       RESW    1
+J       RESW    1
+INDEX   RESW    1
+ARRAY   RESW    81
+SUM     RESW    1
+TEMP    RESW    1
+
+ZERO    WORD    0
+ONE     WORD    1
+TEN     WORD    10
+THREE   WORD    3
